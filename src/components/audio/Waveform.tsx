d507bbc7fae8ceb3124e671d31bbbff2a04ecdf3
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-07-11 23:29:25 -0700

    :zap: FPS limit for waveform line graph

--- src/components/audio/Waveform.tsx
+++ src/components/audio/Waveform.tsx
@@ -2,6 +2,10 @@ import React, { useEffect, useRef, useState } from "react";
 import { getAudioNode } from "../../services/audio/audio-graph";
 import map from "../../utils/map";
 import LineGraph from "../ui/viz/LineGraph";
+import { Stack } from "@chakra-ui/react";
+import NodeInputField from "../nodes/NodeInputField";
+import { useNodeStore } from "../../store/nodes";
+import Slider from "../ui/Slider";
 
 const DEFAULT_AUDIO_HEIGHT = 128;
 
@@ -10,16 +14,37 @@ type Props = {
 };
 
 const Waveform = ({ id }: Props) => {
+  const [fps, setFps] = useState(0);
   const analyser = getAudioNode(id) as AnalyserNode;
 
+  const handleFpsChange = (e: { value: number[] }) => setFps(+e.value);
+
   return (
-    <LineGraph
-      analyser={analyser}
-      color={"blue"}
-      width={400}
-      height={300}
-      animated
-    />
+    <Stack>
+      <LineGraph
+        analyser={analyser}
+        color={"blue"}
+        width={400}
+        height={300}
+        fps={fps}
+        animated
+      />
+
+      <NodeInputField
+        label="Animation Speed (fps)"
+        helper={!fps ? "Unlimited" : `${fps} fps`}
+      >
+        <Slider
+          className="nodrag"
+          min={0}
+          max={120}
+          step={1}
+          defaultValue={[fps]}
+          value={[fps]}
+          onValueChange={handleFpsChange}
+        />
+      </NodeInputField>
+    </Stack>
   );
 };
 
--- src/components/ui/viz/LineGraph.tsx
+++ src/components/ui/viz/LineGraph.tsx
@@ -12,9 +12,16 @@ type Props = BoxProps & {
   analyser: AnalyserNode;
   animated?: boolean;
   color?: ColorPalette;
+  fps?: number;
 };
 
-const LineGraph = ({ analyser, animated, color = "blue", ...props }: Props) => {
+const LineGraph = ({
+  analyser,
+  animated,
+  color = "blue",
+  fps,
+  ...props
+}: Props) => {
   const { colorMode } = useColorMode();
   const bgColor = colorMode === "dark" ? "#111" : "#EEE";
   const lineColor = colorMode === "dark" ? "blue" : "blue";
@@ -23,58 +30,76 @@ const LineGraph = ({ analyser, animated, color = "blue", ...props }: Props) => {
   const animationRef = useRef<ReturnType<typeof requestAnimationFrame> | null>(
     null
   );
+  const prevTime = useRef(0);
 
-  const draw = useCallback(() => {
-    if (!canvasRef.current) return;
-    const canvas = canvasRef.current;
-
-    const ctx = canvas.getContext("2d");
-    if (!ctx) return;
-
-    const canvasWidth = canvas.width;
-    const canvasHeight = canvas.height;
-
-    // Get audio data
-    if (!analyser || !data.current) return;
-
-    // Update waveform data as a ref
-    if (data.current.length == 0) {
-      const newBufferLength = analyser.frequencyBinCount;
-      data.current = new Uint8Array(newBufferLength);
-    }
-    analyser.getByteTimeDomainData(data.current);
-
-    // Clear drawing
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    ctx.fillStyle = bgColor;
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-
-    ctx.beginPath();
-    ctx.lineWidth = 1.5;
-    ctx.strokeStyle = lineColor;
-    for (let i = 0; i < canvasWidth; i++) {
-      const index = Math.floor(map(i, 0, canvasWidth, 0, data.current.length));
-      const x = i;
-      // We scale the audio values to 0-1 to make it easier
-      const amplitude = map(
-        data.current[index],
-        DEFAULT_AUDIO_HEIGHT - 20,
-        DEFAULT_AUDIO_HEIGHT + 20,
-        0,
-        1
-      );
-      const y = (amplitude * canvasHeight) / 2 + canvasHeight / 4;
-      if (i === 0) {
-        ctx.moveTo(x, y);
-      } else {
-        ctx.lineTo(x, y);
+  const draw = useCallback(
+    (now: number) => {
+      // Draw to a specific FPS if needed
+      if (fps && animated) {
+        const fpsInterval = 1000 / fps;
+        const elapsed = now - prevTime.current;
+        // If we haven't elapsed enough time, keep looping
+        if (elapsed < fpsInterval) {
+          return (animationRef.current = requestAnimationFrame(draw));
+        } else {
+          prevTime.current = now - (elapsed % fpsInterval);
+        }
       }
-    }
 
-    ctx.stroke();
+      if (!canvasRef.current) return;
+      const canvas = canvasRef.current;
 
-    if (animated) animationRef.current = requestAnimationFrame(draw);
-  }, [data, lineColor, bgColor, animated]);
+      const ctx = canvas.getContext("2d");
+      if (!ctx) return;
+
+      const canvasWidth = canvas.width;
+      const canvasHeight = canvas.height;
+
+      // Get audio data
+      if (!analyser || !data.current) return;
+
+      // Update waveform data as a ref
+      if (data.current.length == 0) {
+        const newBufferLength = analyser.frequencyBinCount;
+        data.current = new Uint8Array(newBufferLength);
+      }
+      analyser.getByteTimeDomainData(data.current);
+
+      // Clear drawing
+      ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+      ctx.fillStyle = bgColor;
+      ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+
+      ctx.beginPath();
+      ctx.lineWidth = 1.5;
+      ctx.strokeStyle = lineColor;
+      for (let i = 0; i < canvasWidth; i++) {
+        const index = Math.floor(
+          map(i, 0, canvasWidth, 0, data.current.length)
+        );
+        const x = i;
+        // We scale the audio values to 0-1 to make it easier
+        const amplitude = map(
+          data.current[index],
+          DEFAULT_AUDIO_HEIGHT - 20,
+          DEFAULT_AUDIO_HEIGHT + 20,
+          0,
+          1
+        );
+        const y = (amplitude * canvasHeight) / 2 + canvasHeight / 4;
+        if (i === 0) {
+          ctx.moveTo(x, y);
+        } else {
+          ctx.lineTo(x, y);
+        }
+      }
+
+      ctx.stroke();
+
+      if (animated) animationRef.current = requestAnimationFrame(draw);
+    },
+    [data, lineColor, bgColor, animated, fps]
+  );
 
   useEffect(() => {
     animationRef.current = requestAnimationFrame(draw);
@@ -82,7 +107,7 @@ const LineGraph = ({ analyser, animated, color = "blue", ...props }: Props) => {
     return () => {
       if (animationRef.current) cancelAnimationFrame(animationRef.current);
     };
-  }, [draw, lineColor, bgColor]);
+  }, [draw, lineColor, bgColor, fps]);
 
   return <Box as="canvas" ref={canvasRef} {...props} />;
 };
