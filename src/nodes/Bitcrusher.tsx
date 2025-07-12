8fface71759eca04362868ae5c656df93c389919
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-07-12 00:10:27 -0700

    :zap: syncs ui with worklet wasm

--- src/nodes/Bitcrusher.tsx
+++ src/nodes/Bitcrusher.tsx
@@ -1,5 +1,5 @@
 import { Position, type Node } from "@xyflow/react";
-import React from "react";
+import React, { useState } from "react";
 import { useNodeStore } from "../store/nodes";
 import NodeContainer from "../components/nodes/NodeContainer";
 import NodeHeading from "../components/nodes/NodeHeading";
@@ -9,6 +9,7 @@ import NodeInputField from "../components/nodes/NodeInputField";
 import NodeHandle from "../components/nodes/NodeHandle";
 import NodeInput from "../components/nodes/NodeInput";
 import NodeOutput from "../components/nodes/NodeOutput";
+import { getAudioSetup } from "../services/audio/audio-graph";
 
 export type ConstantSourceData = {
   offset: number;
@@ -20,11 +21,24 @@ type Props = {
 };
 
 const Bitcrusher = ({ id, data }: Props) => {
-  const { updateNode } = useNodeStore();
+  // This uses a AudioWorklet that uses WASM under the hood
+  // So we need to access the wrapper instance to use it's methods to set properties
+  // @TODO: Maybe leverage direct port messages in `updateNode()`?
+  const bitcrusherInstance = getAudioSetup(id)?.instance;
+  const [bits, setBits] = useState(4);
+  const [normfreq, setNormfreq] = useState(0.1);
 
-  //   const setOffset = (e: { value: number[] }) => {
-  //     updateNode<ConstantSourceData>(id, { offset: +e.value });
-  //   };
+  const handleBits = (e: { value: number[] }) => {
+    const newValue = +e.value;
+    bitcrusherInstance?.setBits(newValue);
+    setBits(newValue);
+  };
+
+  const handleNormfreq = (e: { value: number[] }) => {
+    const newValue = +e.value;
+    bitcrusherInstance?.setNormfreq(newValue);
+    setNormfreq(newValue);
+  };
 
   return (
     <NodeContainer>
@@ -32,16 +46,34 @@ const Bitcrusher = ({ id, data }: Props) => {
       <NodeInput />
       <NodeOutput />
       <NodeContent>
-        {/* <NodeInputField label="Offset" helper={`${data.offset}`}>
+        <NodeInputField
+          label="Bits"
+          helper={`${bits}`}
+          tooltip="The resolution. Lower = Crunchier"
+        >
+          <Slider
+            className="nodrag"
+            min={0}
+            max={16}
+            step={1}
+            value={[bits]}
+            onValueChange={handleBits}
+          />
+        </NodeInputField>
+        <NodeInputField
+          label="Normfreq"
+          helper={`${normfreq}`}
+          tooltip="0 = more effect, 1 = less effect"
+        >
           <Slider
             className="nodrag"
             min={0}
             max={1}
             step={0.01}
-            value={[data.offset]}
-            onValueChange={setOffset}
+            value={[normfreq]}
+            onValueChange={handleNormfreq}
           />
-        </NodeInputField> */}
+        </NodeInputField>
       </NodeContent>
     </NodeContainer>
   );
