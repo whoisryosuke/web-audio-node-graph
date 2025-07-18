68e839d22fcb2107e932fc8e6d0d9b6539382113
Ryosuke Hana <ryosuke.san.hana@gmail.com>
2025-07-17 22:59:05 -0700

    :zap: useful for local dev

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
+  server: {
+    fs: {
+      // Allow serving files from one level up from the project root
+      allow: ["..", "./node_modules"],
+    },
+  },
+
   optimizeDeps: {
     exclude: ["clawdio"],
   },
